d6a72602423c1e2651e48fc39af1523040f601cc
Cristian Anca <cristian.anca@gmail.com>
2017-07-17 12:56:59 +0300

    Added register form and base template

--- message/views.py
+++ message/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.edit import CreateView
+
+from message.forms import RegisterForm
+
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'register.html'
