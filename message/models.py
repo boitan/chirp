65e71b05156e8de283fc05f66853a7a1c2eea0f8
Cristian Anca <cristian.anca@gmail.com>
2017-07-17 11:14:04 +0300

    Added models to the app

--- message/models.py
+++ message/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TimestampModel(models.Model):
+    class Meta:
+        abstract = True
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Message(TimestampModel):
+    user = models.ForeignKey(User)
+    status = models.TextField(null=False, max_length=140, blank=False)
+
+
+class Like(TimestampModel):
+    user = models.ForeignKey(User)
+    message =models.ForeignKey(Message)
+    like = models.BooleanField()
+
+
+class Follow(TimestampModel):
+    followed_user = models.ForeignKey(User, related_name='followed_by')
+    following_user = models.ForeignKey(User, related_name='following')
